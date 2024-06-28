f0be37bc7256095d01a8bddc53e087a1250d703b
Bruno <brutusmt0811@gmail.com>
2024-06-28 18:11:44 -0300

    cadastro conectado ao banco de dados

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-import secrets, json, re, random, string
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
+import random, string
 from mysql.connector import connect
-from sqlalchemy.orm import relationship
-
 
 app = Flask(__name__, template_folder = 'pages')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
 
 db = connect(
       user = 'root',
@@ -15,33 +10,11 @@ db = connect(
       host = '127.0.0.1',
       database = 'SouSeleto')
 
-secret_key = secrets.token_hex(32)
-
-app.config['SECRET_KEY'] = secret_key
-
-#app.app_context().push()
-#db = SQLAlchemy(app)
-
-"""class Aluno(db.Model):
-    id = db.Column('studentId',db.Integer, primary_key=True)
-    nome = db.Column(db.String(200))
-    info1 = db.Column(db.String(100))
-    info2 = db.Column(db.String(100))
-    info3 = db.Column(db.String(20))
-
-    def __init__(self,nome_aux,info1_aux,info2_aux,info3_aux):
-        self.nome = nome_aux
-        self.info1 = info1_aux
-        self.info2 = info2_aux
-        self.info3 = info3_aux"""
-
-def random_senha():
-    random_senha = ''.join(random.choice(string.ascii_letters) for i in range(6))
 
 @app.route("/")
 def main_page():
     cursor = db.cursor()
-    search = 'select idCadastro, nomeCadastro, nascimentoCadastro from cadastros limit 15'
+    search = 'select idCadastro, nomeCadastro, nascimentoCadastro from cadastros limit 13'
     cursor.execute(search)
     results = cursor.fetchall()
     print("yu")
@@ -58,21 +31,24 @@ def show_cad():
 
 @app.route("/<cadastro>", methods=['GET', 'POST'])
 def get_cad(cadastro):
+    link = request.base_url
     cursor = db.cursor()
-    search = 'select * from cadastros where idCadastro = %s'
-    cursor.execute(search, tuple(cadastro))
-    results = cursor.fetchall()
+    if(link == 'http://127.0.0.1:3000/usuarios'):
+        search = 'select * from usuarios where idUsuario = %s'
+        cursor.execute(search, tuple(cadastro))
+    else:
+        search = 'select * from cadastros where idCadastro = %s'
+        cursor.execute(search, tuple(cadastro))
+        results = cursor.fetchall()
 
     return render_template('index.html', cad=results)
 
 @app.route("/usuarios", methods=['GET', 'POST'])
 def get_users():
     cursor = db.cursor()
-    search = 'select idUsuario, nomeUsuario, nascimentoUsuario from usuarios'
+    search = 'select * from usuarios where idUsuario'
     cursor.execute(search)
     results = cursor.fetchall()
-    print("oi")
-    print(results)
 
     return render_template('index.html', info=results)
 
@@ -80,8 +56,8 @@ def get_users():
 def new_cad():
     if request.method == 'POST':
         cursor = db.cursor()
-        add = 'insert into cadastros (nomeCadastro, cpfCadastro, nascimentoCadastro, nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai, dataEntrada, tipoSanguineo) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        info = request.form['nome'], request.form['info1'], request.form['info2'], request.form['info3'], request.form['info4'], request.form['info5'], request.form['info6'], request.form['info7'], request.form['info8'], request.form['info9']
+        add = 'insert into cadastros (nomeCadastro, cpfCadastro, nascimentoCadastro, nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai, dataEntrada, tipoSanguineo, celular, telefone) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        info = request.form['info0'], request.form['info1'], request.form['info2'], request.form['info3'], request.form['info4'], request.form['info5'], request.form['info6'], request.form['info7'], request.form['info8'], request.form['info9'], request.form['info10'], request.form['info10']
         cursor.execute(add, info)
         db.commit()
 
@@ -116,8 +92,6 @@ def login_user():
             info = request.form['cpfUser']
             cursor.execute(search, info)
             results = cursor.fetchall()
-            print('oi')
-            print(results)
 
             return redirect(url_for('login_user'))
 
@@ -126,29 +100,26 @@ def login_user():
 
 @app.route("/update/<cad>", methods = ['GET','POST'])
 def update_cad(cad):
-    print("yahu")
     if request.method == 'POST':
-        print("guhh")
-
         cursor = db.cursor()
-        add = 'update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s, nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s where idCadastro = %s'
-        info = request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['mae'], request.form['pai'], request.form['resp'], request.form['cpf2'], request.form['rg'], request.form['tiposangue'], cad
+        add = 'update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s, nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s, telefone = %s, celular = %s where idCadastro = %s'
+        info = request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['mae'], request.form['pai'], request.form['resp'], request.form['cpf2'], request.form['rg'], request.form['tiposangue'], request.form['telefone'], request.form['celular'], cad
         cursor.execute(add, info)
         db.commit()
-        print("brabo")
 
         return redirect(url_for('main_page'))
 
     return render_template('index.html')
 
-@app.route("/delete/<int:id>")
+@app.route("/delete/<id>")
 def delete(id):
-    aluno = Aluno.query.get(id)
-    db.session.delete(aluno)
-    db.session.commit()
-    return redirect(url_for('main_page'))
+    cursor = db.cursor()
+    remove = 'delete from cadastros where idCadastro = %s'
+    cursor.execute(remove, tuple(id))
+    db.commit()
 
+    return redirect(url_for('main_page'))
 
 if __name__ == '__main__':
 
-    app.run(port=3000, debug=True)
+    app.run(port=3000, debug=False)
--- static/index.js
+++ static/index.js
@@ -1,45 +1,42 @@
 window.onload = function togglePopup(){
     link = window.location.href;
-    if(link == 'http://127.0.0.1:3000/1'){
+    if(link != 'http://127.0.0.1:3000/' && link != 'http://127.0.0.1:3000/usuarios'){
     const overlay = document.getElementById('overlay');
     overlay.classList.toggle('show');
-    }   else{
+    }
+    else{
             return 0;
         }
 }
-
-
 
 function closePopup(){
     const overlay = document.getElementById('overlay');
     overlay.classList.remove('show');
 }
 
- function validateForm(event) {
+function validateForm(event) {
             var submitButton = event.submitter;
 
             // Verifica se o botão clicado foi o de Submit
-            if (submitButton && submitButton.value === 'Submit') {
+
                 // Verifica se todos os campos estão preenchidos
                 var inputs = document.querySelectorAll('.info input');
                 for (var i = 0; i < inputs.length; i++) {
-                    if (inputs[i].value.trim() === '' && i != 6 && i != 7) {
+                    if (inputs[i].value.trim() === '') {
                         // Mostra a mensagem de aviso
                         document.getElementById('warningMessage').style.display = 'block';
                         return false; // Impede o envio do formulário
                     }
                 }
-            }
+
 
             return true; // Permite o envio do formulário se todos os campos estiverem preenchidos
         }
 
-        function goBack() {
-            // Função para voltar (simulada)
+function goBack() {
+   window.history.back();
+}
 
-            // Exemplo de redirecionamento
-            window.history.back();
-        }
 function toggleLogin(){
     const overlay = document.getElementById('login');
     overlay.classList.toggle('show');
