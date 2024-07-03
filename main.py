30c009c783eff4a258d5bf2698bbd4e759886343
Bruno <brutusmt0811@gmail.com>
2024-07-03 18:30:00 -0300

    cadastro conectado ao banco de dados

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import random, string
+
+from jinja2.nodes import Keyword
 from mysql.connector import connect
 
 app = Flask(__name__, template_folder = 'pages')
@@ -17,7 +19,7 @@ def main_page():
     try:
         if request.method == 'POST':
             searchbar = request.form.get('searchbar')
-            search = 'select idCadastro, nomeCadastro, nascimentoCadastro from cadastros where nomeCadastro like "%%%s%%"'
+            search = "select idCadastro, nomeCadastro, nascimentoCadastro from cadastros where nomeCadastro like '%s'"
             cursor.execute(search, searchbar)
             print(searchbar)
         else:
@@ -25,7 +27,7 @@ def main_page():
             search = 'select idCadastro, nomeCadastro, nascimentoCadastro from cadastros limit 13'
             cursor.execute(search)
     except:
-        print('oops')
+        print("dead")
 
     results = cursor.fetchall()
     print(search)
@@ -37,13 +39,13 @@ def main_page():
         return render_template('new.html', info=results, cad='o')'''
     return render_template('index.html', info=results, cad='o')
 
-def show_cad():
+'''def show_cad():
     cursor = db.cursor()
     search = 'select * from cadastros'
-    cursor.execute(search)
+    print(cursor.execute(search))
     results = cursor.fetchall()
 
-    return render_template('index.html', blob='')
+    return render_template('index.html', blob='')'''
 
 @app.route("/<cadastro>", methods=['GET', 'POST'])
 def get_cad(cadastro):
@@ -72,7 +74,7 @@ def get_users():
 def new_cad():
     if request.method == 'POST':
         cursor = db.cursor()
-        add = 'insert into cadastros (nomeCadastro, cpfCadastro, nascimentoCadastro, nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai, dataEntrada, tipoSanguineo, celular, telefone) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        add = 'insert into cadastros (nomeCadastro, cpfCadastro, nascimentoCadastro, nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai, dataEntrada, tipoSanguineo, celularResponsavel, telefoneResponsavel) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         info = request.form['info0'], request.form['info1'], request.form['info2'], request.form['info3'], request.form['info4'], request.form['info5'], request.form['info6'], request.form['info7'], request.form['info8'], request.form['info9'], request.form['info10'], request.form['info10']
         cursor.execute(add, info)
         db.commit()
@@ -118,7 +120,7 @@ def login_user():
 def update_cad(cad):
     if request.method == 'POST':
         cursor = db.cursor()
-        add = 'update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s, nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s, telefone = %s, celular = %s where idCadastro = %s'
+        add = 'update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s, nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s, telefoneResponsavel = %s, celularResponsavel = %s where idCadastro = %s'
         info = request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['mae'], request.form['pai'], request.form['resp'], request.form['cpf2'], request.form['rg'], request.form['tiposangue'], request.form['telefone'], request.form['celular'], cad
         cursor.execute(add, info)
         db.commit()
