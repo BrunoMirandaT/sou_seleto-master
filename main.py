e3ec4291ed9988808eef34e98930910a0fcbf1be
Bruno <brutusmt0811@gmail.com>
2024-07-05 22:13:46 -0300

    cadastro conectado ao banco de dados

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import random, string
 
-from jinja2.nodes import Keyword
 from mysql.connector import connect
 
 app = Flask(__name__, template_folder = 'pages')
 
-db = connect(
+db = connect( # Configuração da conexão com banco de dados
       user = 'root',
       password = '',
       host = '127.0.0.1',
@@ -32,56 +31,46 @@ def main_page():
     results = cursor.fetchall()
     print(search)
     print(results)
-    '''idUser = 1
-    if idUser == 1:
-        return render_template('index.html', info=results, cad='o')
-    else:
-        return render_template('new.html', info=results, cad='o')'''
     return render_template('index.html', info=results, cad='o')
 
-'''def show_cad():
-    cursor = db.cursor()
-    search = 'select * from cadastros'
-    print(cursor.execute(search))
-    results = cursor.fetchall()
 
-    return render_template('index.html', blob='')'''
-
-@app.route("/<cadastro>", methods=['GET', 'POST'])
+@app.route("/cadastro/<cadastro>", methods=['GET', 'POST'])
 def get_cad(cadastro):
-    link = request.base_url
     cursor = db.cursor()
-    if(link == 'http://127.0.0.1:3000/usuarios'):
-        search = 'select * from usuarios where idUsuario = %s'
-        cursor.execute(search, tuple(cadastro))
-    else:
-        search = 'select * from cadastros where idCadastro = %s'
-        cursor.execute(search, tuple(cadastro))
-        results = cursor.fetchall()
+    search = 'select * from cadastros where idCadastro = %s'
+    cursor.execute(search, tuple(cadastro))
+    results = cursor.fetchall()
 
     return render_template('index.html', cad=results)
 
 @app.route("/usuarios", methods=['GET', 'POST'])
-def get_users():
-    cursor = db.cursor()
-    search = 'select * from usuarios'
-    cursor.execute(search)
-    results = cursor.fetchall()
+def list_users():
+    cursor = db.cursor() # Abre conexão com o banco de dados
+    search = 'select * from usuarios' # Comando sql utilizado para pegar usuários do banco de dados
+    cursor.execute(search) # executa comando sql acima
+    results = cursor.fetchall() # salva resultado do comando acima em uma variável
 
-    return render_template('index.html', info=results)
+    return render_template('users.html', info=results) # Renderiza página de lista de usuários,
+                                                                        # passando resultado de pesquisa sql para exibição no html
 
-@app.route("/new", methods=['GET', 'POST'])
+@app.route("/cadastros/novo", methods=['GET', 'POST'])
 def new_cad():
     if request.method == 'POST':
-        cursor = db.cursor()
-        add = 'insert into cadastros (nomeCadastro, cpfCadastro, nascimentoCadastro, nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai, dataEntrada, tipoSanguineo, celularResponsavel, telefoneResponsavel) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        info = request.form['info0'], request.form['info1'], request.form['info2'], request.form['info3'], request.form['info4'], request.form['info5'], request.form['info6'], request.form['info7'], request.form['info8'], request.form['info9'], request.form['info10'], request.form['info10']
-        cursor.execute(add, info)
-        db.commit()
+        cursor = db.cursor() # Abre conexão com o banco de dados
+        add = ('insert into cadastros(nomeCadastro, cpfCadastro, nascimentoCadastro,'
+               ' nomeResponsavel, cpfResponsavel, rgResponsavel, nomeMae, nomePai,'
+               ' dataEntrada, tipoSanguineo, celularResponsavel, telefoneResponsavel)'
+               ' values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)') # Comando sql utilizado para inserir cadastro no banco de dados
+        info = (request.form['info0'], request.form['info1'], request.form['info2'],
+                request.form['info3'], request.form['info4'], request.form['info5'],
+                request.form['info6'], request.form['info7'], request.form['info8'],
+                request.form['info9'], request.form['info10'], request.form['info10']) # Pega todas as informações do cadastro e insere em uma variavel
+        cursor.execute(add, info) # Executa comando sql junto com informações acima
+        db.commit() # Envia mudanças para o BD
 
-        return redirect(url_for('main_page'))
+        return redirect(url_for('main_page')) # Retorna para rota main_page
 
-    return render_template('new.html')
+    return render_template('new.html') # Renderiza página de cadastro
 
 @app.route("/new", methods=['GET', 'POST'])
 def new_user():
@@ -116,12 +105,29 @@ def login_user():
     return render_template('index.html')
 
 
-@app.route("/update/<cad>", methods = ['GET','POST'])
+@app.route("/update/<cad>", methods = ['POST'])
 def update_cad(cad):
     if request.method == 'POST':
+        cursor = db.cursor() # Abre conexão com o banco de dados
+        add = ('update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s,'
+               ' nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s,'
+               ' telefoneResponsavel = %s, celularResponsavel = %s where idCadastro = %s') # Comando sql utilizado para atualizar cadastro no banco de dados
+        info = (request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['mae'],
+                request.form['pai'], request.form['resp'], request.form['cpf2'], request.form['rg'],
+                request.form['tiposangue'], request.form['telefone'], request.form['celular'], cad) # Pega todas as informações do cadastro e insere em uma variavel
+        cursor.execute(add, info) # Executa comando sql junto com informações acima
+        db.commit() # Envia mudanças para o BD
+
+        return redirect(url_for('main_page')) # Retorna para rota main_page
+
+    return render_template('index.html') # Renderiza página de cadastro
+
+@app.route("/update/<user>", methods = ['GET','POST'])
+def update_user(user):
+    if request.method == 'POST':
         cursor = db.cursor()
-        add = 'update cadastros set nomeCadastro = %s, cpfCadastro = %s, nascimentoCadastro = %s, nomeMae = %s, nomePai = %s, nomeResponsavel = %s, cpfResponsavel = %s, rgResponsavel = %s, tipoSanguineo = %s, telefoneResponsavel = %s, celularResponsavel = %s where idCadastro = %s'
-        info = request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['mae'], request.form['pai'], request.form['resp'], request.form['cpf2'], request.form['rg'], request.form['tiposangue'], request.form['telefone'], request.form['celular'], cad
+        add = 'update usuarios set nomeUsuario = %s, cpfUsuario = %s, nascimentoUsuario = %s, celularUsuario = %s, nvlAcesso = %s where idCadastro = %s'
+        info = request.form['nome'], request.form['cpf'], request.form['nasc'], request.form['celular'], request.form['nivel'], user
         cursor.execute(add, info)
         db.commit()
 
@@ -129,14 +135,23 @@ def update_cad(cad):
 
     return render_template('index.html')
 
-@app.route("/delete/<id>")
-def delete(id):
+@app.route("/delete/cadastros/<id>")
+def delete_cad(id):
     cursor = db.cursor()
     remove = 'delete from cadastros where idCadastro = %s'
     cursor.execute(remove, tuple(id))
     db.commit()
 
     return redirect(url_for('main_page'))
+
+@app.route("/delete/usuarios/<id>")
+def delete_user(id):
+    cursor = db.cursor() # Abre conexão com o banco de dados
+    remove = 'delete from usuarios where idUsuario = %s' # Comando sql utilizado para deletar usuário especifico no banco de dados
+    cursor.execute(remove, tuple(id)) # Executa comando sql junto com o id do usuário que sera deletado
+    db.commit() # Envia mudanças para o BD
+
+    return redirect(url_for('list_users')) # Retorna para rota list_users
 
 if __name__ == '__main__':
 
